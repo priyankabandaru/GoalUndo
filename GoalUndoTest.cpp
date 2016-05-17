452b636e6a3894db627bae5120a0a970233a0194
priyankabandaru <pbandaru@mail.csuchico.edu>
2016-05-17 16:18:23 -0700

    Test cases Updated

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -19,7 +19,7 @@ TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
-TEST(GoalUndoTest, addgoalndoperation)
+TEST(GoalUndoTest, addgoal_ndoperation)
 {
 	GoalUndo test;
 	test.addOperation("Copy","Selec");
@@ -27,51 +27,86 @@ TEST(GoalUndoTest, addgoalndoperation)
 TEST(GoalUndoTest, addoperation)
 {
 	GoalUndo test;
+  test.addOperation("Copy","Selec");
 	test.addOperation("Cp");
 }
 TEST(GoalUndoTest, addgoalndoperation2)
 {
 	GoalUndo test;
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
 	test.addOperation("Delete","Select");
 }
 TEST(GoalUndoTest, addoperation2)
 {
 	GoalUndo test;
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+	test.addOperation("Delete","Select");
 	test.addOperation("del");
 }
 TEST(GoalUndoTest, getoperation)
 {
 	GoalUndo test;
-  int check;
-	std::string goals=test.getOperations();
-  std::string expected= "Select del";
-  if(goals==expected)
-  {
-    check=0;
-  }
-  //int check=expected.compare(goals);
-  ASSERT_EQ(check,0);
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.addOperation("Delete","Select");
+  test.addOperation("del");
+  ASSERT_EQ("Select del",test.getOperations());
 }
 TEST(GoalUndoTest, getgoal)
 {
 	GoalUndo test;
-	std::string goals=test.getGoal();
-  std::string expected="Delete";
-  int check=expected.compare(goals);
-  ASSERT_EQ(check,0);
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.addOperation("Delete","Select");
+  test.addOperation("del");
+  ASSERT_EQ("Delete",test.getGoal());
 }
 TEST(GoalUndoTest, undoOperation)
 {
   GoalUndo test;
-  test.undoOperation("Select");
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.addOperation("Delete","Select");
+  test.addOperation("del");
+  test.undoOperation("Selec");
+  test.undoGoal();
+  ASSERT_EQ("Cp",test.getOperations());
+
 }
 TEST(GoalUndoTest, undoOperation1)
 {
   GoalUndo test;
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.addOperation("Delete","Select");
+  test.addOperation("del");
   test.undoOperation();
+  ASSERT_EQ("Select",test.getOperations());
 }
+
 TEST(GoalUndoTest, undoGoal)
 {
   GoalUndo test;
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.addOperation("Delete","Select");
+  test.addOperation("del");
   test.undoGoal();
+  ASSERT_EQ("Copy",test.getGoal());
+}
+TEST(GoalUndoTest,undoGoalEmpty)
+{
+  GoalUndo test;
+  ASSERT_EQ("",test.getGoal());
+}
+TEST(GoalUndoTest,goalwithlastoperationdel)
+{
+  GoalUndo test;
+  test.addOperation("Copy","Selec");
+  test.addOperation("Cp");
+  test.undoOperation();
+  test.undoOperation();
+  ASSERT_EQ("",test.getGoal());
 }
